b2dec8ab8e62798be89e81b0bbcb3efd4386a999
RakshitGumber <gumberrakshit@gmail.com>
2026-06-18 09:24:39 +0530

    added functions and learned how to make multiple function work

--- apps/cli/main.py
+++ apps/cli/main.py
@@ -1,8 +1,14 @@
 import typer
 
-app = typer.Typer()
-app.command()
+from functions.weather.weather import weather
+from functions.greet.greet import greet
+from functions.health.doctor import doctor
 
+app = typer.Typer()
+
+app.command()(weather)
+app.command()(greet)
+app.command()(doctor)
 
 if __name__ == "__main__":
     app()
---
+++ functions/health/doctor.py
@@ -0,0 +1,2 @@
+def doctor():
+    print("Everything looks fine")
