cf387471dda4adc21f100d66f6b2a2709a297b9a
RakshitGumber <gumberrakshit@gmail.com>
2026-06-19 22:33:45 +0530

    I am able to fetch the weather

--- apps/cli/main.py
+++ apps/cli/main.py
@@ -1,5 +1,21 @@
 import logging
 from collections import deque
+import requests
+
+from dotenv import load_dotenv
+import os
+
+from rich.console import Console
+from rich.panel import Panel
+
+console = Console()
+
+from rich.table import Table
+
+load_dotenv()
+
+WEATHER_API = os.getenv("WEATHER_API")
+BASE_URL = "https://api.weatherapi.com/v1"
 
 import typer
 
@@ -34,11 +50,33 @@ def status():
 @weather_app.command()
 def current():
     try:
-        raise ValueError("Failed to fetch weather")
+        url = f"{BASE_URL}/current.json?key={WEATHER_API}&q=delhi&aqi=no"
+        response = requests.get(url)
+        if response.status_code == 200:
+            data = response.json()
+            display_weather(data)
+        else:
+            print(f"Server returned status code: {response.status_code}")
+            raise ValueError("Unable to fetch result")
     except Exception as e:
         print("Ran into an error x_x")
         log_error(str(e))
         raise typer.Exit(code=1)
+
+
+def display_weather(data):
+    current = data["current"]
+
+    table = Table(show_header=False)
+    table.add_row("☀ Condition", current["condition"]["text"])
+    table.add_row("🌡 Temperature", f'{current["temp_c"]}°C')
+    table.add_row("💧 Humidity", f'{current["humidity"]}%')
+    table.add_row("💨 Wind", f'{current["wind_kph"]} km/h')
+    table.add_row("☀ UV Index", str(current["uv"]))
+    table.add_row("👁 Visibility", f'{current["vis_km"]} km')
+    table.add_row("🌧 Rain Chance", f'{current["chance_of_rain"]}%')
+
+    console.print(table)
 
 
 @app.command()
