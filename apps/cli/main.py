e0db624770b40bf5bd018b193997c8678c88b9c5
RakshitGumber <gumberrakshit@gmail.com>
2026-06-19 18:25:14 +0530

    making this app expandable

--- apps/cli/main.py
+++ apps/cli/main.py
@@ -1,14 +1,24 @@
 import typer
 
-from functions.weather.run import weather
-from functions.greet.run import greet
-from functions.health.run import health
+# from commands.weather.run import weather
+# from commands.greet.run import greet
+# from commands.health.run import health
 
 app = typer.Typer()
 
-app.command()(weather)
-app.command()(greet)
-app.command()(health)
+health_app = typer.Typer()
+
+
+def status():
+    print("Working: OK")
+
+
+health_app.command()(status)
+
+
+# app.command()(weather)
+# app.command()(greet)
+app.add_typer(health_app, name="health")
 
 if __name__ == "__main__":
     app()
---
+++ commands/weather/run.py
@@ -0,0 +1,9 @@
+BASE_URL = "api.weatherapi.com/v1"
+
+
+def weather():
+    print("today is good")
+
+
+if __name__ == "__main__":
+    weather()
